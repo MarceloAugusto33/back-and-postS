9a5a41969f1507d2716294975a8826181f105bbf
marcelo <sjsjsjmarcelo@gmail.com>
2024-04-28 14:36:28 -0300

    feat: cadastra usuario no banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import 'dotenv/config'
 import express, { urlencoded } from 'express';
 import cors from 'cors'
+import { router } from './routes';
 
 export class Server {
     private server = express()
 
     constructor() {
         this.server = express();
+
+        this.configuredServer()
+        this.setRoutes()
 
         this.server.listen(process.env.SERVER_PORT, () => {
             console.log("Server running in port" + process.env.SERVER_PORT)
@@ -19,5 +23,7 @@ export class Server {
         this.server.use(cors())
     }
 
-    
+    setRoutes() {
+        this.server.use(router)
+    }    
 }
